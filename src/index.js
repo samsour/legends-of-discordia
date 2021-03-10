98011cf8c90a902462645173c87fd92781c9d5be
Niklas Maier <niklas.maier@flowd.de>
2021-03-10 23:46:51 +0100

    Refactor classnames + remove old sample code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import DiscordClient from './client.js';
-import CommandRegistry from './command/registry.js';
 import dotenv from 'dotenv';
-import DatabaseConnectionFactory from './database/databaseConnectionFactory.js';
+import DiscordClient from './Client.js';
+import CommandRegistry from './command/CommandRegistry.js';
+import DatabaseConnectionFactory from './factory/DatabaseConnectionFactory.js';
 
 // read .env vars into 'process.env'
 dotenv.config();
 
 // setup database connection
-DatabaseConnectionFactory.connect();
+DatabaseConnectionFactory.connect()
+    .catch(() => {
+        process.exit(1);
+    });
+
 
 // setup application
 new CommandRegistry();
