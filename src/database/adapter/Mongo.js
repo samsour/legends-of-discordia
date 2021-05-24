ac0ba45b92652ab67c16b6e41d6b215f6183131a
Niklas Maier <niklas.maier@flowd.de>
2021-05-24 11:16:10 +0200

    Fix server crash by try-catch around mongoose connection esstablish
    Summary:
    The server used to crash 'randomly' without useful error-logging.
    This was because mongoose could not establish a database connection
    via the provided url in `process.env.MONGO_PATH`. By fixing the url
    when running in node docker container it is no fixed when run via
    docker-compose.
    The try-catch now also loggs a detailed error message.

--- src/database/adapter/Mongo.js
+++ src/database/adapter/Mongo.js
@@ -1,12 +1,17 @@
 import mongoose from 'mongoose';
 
 export default async () => {
-    await mongoose.connect(process.env.MONGO_PATH, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        dbName: process.env.MONGO_DB_NAME,
-        user: process.env.MONGO_USER,
-        pass: process.env.MONGO_PASSWORD,
-    });
+    try {
+        await mongoose.connect(process.env.MONGO_PATH, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            dbName: process.env.MONGO_DB_NAME,
+            user: process.env.MONGO_USER,
+            pass: process.env.MONGO_PASSWORD,
+        });
+    } catch (error) {
+        console.log('1621847075 MongoDB connection error:');
+        console.error(error);
+    }
     return mongoose;
 };
