32dbed602e0b8e84512af24b5f66f6418e5ea7be
Niklas Maier <niklas.maier@flowd.de>
2021-05-24 22:04:47 +0200

    Add tests

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ start:
 	docker-compose up -d --force-recreate
 
 run-tests:
-	docker-compose run node npm run test
+	docker-compose run node npm run test $(ARGUMENTS)
 
 npm:
 	docker-compose run node npm $(ARGUMENTS)
--- __tests__/unit/commands/BaseCommand.test.js
+++ __tests__/unit/commands/BaseCommand.test.js
@@ -1,8 +1,27 @@
+import { expect } from '@jest/globals';
 import BaseCommand from '../../../src/commands/BaseCommand.js';
 import NotImplementedError from '../../../src/error/NotImplementedError.js';
 
-
 describe('BaseCommand', () => {
+    test('.constructor() sets config object', () => {
+        const baseCommand = new BaseCommand(['lorem', 'i'], {additional: 'options', lorem: 'ipsum'});
+
+        expect(baseCommand.config).toEqual({
+            'additional': 'options',
+            'aliases': [
+                'lorem',
+                'i'
+            ],
+            'expectedArgs': '',
+            'lorem': 'ipsum',
+            'maxArgs': null,
+            'minArgs': 0,
+            'permissionError': 'You do not have permission to run this command.',
+            'permissions': [],
+            'requiredRoles': []
+        });
+    });
+
     test('.execute() throws NotImplementedException', () => {
         const baseCommand = new BaseCommand();
         expect(() => baseCommand.execute()).toThrowError(NotImplementedError);
